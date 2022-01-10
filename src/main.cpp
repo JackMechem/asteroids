d29e18e2f766718c7336a09397ebc9393106e303
JackMechem <mechemjack@gmail.com>
2022-01-09 22:45:28 -0800

    Moveable Player

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,56 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(300, 200), "Asteroids");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Blue);
+
+    int windowx = 512;
+    int windowy = 512;
+
+    float playerx = 60.0f;
+    float playery = 100.0f;
+
+    sf::RenderWindow window(sf::VideoMode(windowx, windowy), "Asteroids", sf::Style::Close | sf::Style::Resize);
+
+    sf::RectangleShape player(sf::Vector2f(playerx, playery));
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("images/player.png");
+    player.setTexture(&playerTexture);
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
         {
-            if (event.type == sf::Event::Closed)
+
+            switch (evnt.type)
+            {
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::Resized:
+                sf::FloatRect visibleArea(0, 0, evnt.size.width, evnt.size.height);
+                window.setView(sf::View(visibleArea));
+                break;
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
+            player.move(-0.1f, 0.0f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
+            player.move(0.0f, -0.1f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
+            player.move(0.1f, 0.0f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
+            player.move(0.0f, 0.1f);
+        }
+
+        window.clear(sf::Color(0, 0, 0));
+        window.draw(player);
         window.display();
     }
 
